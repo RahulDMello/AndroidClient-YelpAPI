fc27c814a261db3cbbb88223e906436b84f9ab6b
anonDfeline <rvdmello96@gmail.com>
2020-03-04 06:03:08 -0500

    Add Up button in options menu

--- AndroidClientYelpAPI/app/src/main/java/com/example/androidclientyelpapi/MainActivity.kt
+++ AndroidClientYelpAPI/app/src/main/java/com/example/androidclientyelpapi/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.androidclientyelpapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
