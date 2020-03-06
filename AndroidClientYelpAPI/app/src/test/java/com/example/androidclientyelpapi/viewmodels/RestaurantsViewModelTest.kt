9765055dff0ffc6df8ce969f2eb50c1526f34fed
anonDfeline <rvdmello96@gmail.com>
2020-03-05 20:45:25 -0500

    - Setup for unit tests

---
+++ AndroidClientYelpAPI/app/src/test/java/com/example/androidclientyelpapi/viewmodels/RestaurantsViewModelTest.kt
@@ -0,0 +1,40 @@
+package com.example.androidclientyelpapi.viewmodels
+
+import com.example.androidclientyelpapi.service.RestaurantsRepository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RestaurantsViewModelTest {
+
+    @Before
+    fun setUp() {
+        RestaurantsRepository
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun updateRestaurantList() {
+    }
+
+    @Test
+    fun fetchReview() {
+    }
+
+    @Test
+    fun fetchFavourites() {
+    }
+
+    @Test
+    fun updateFavourite() {
+    }
+
+    @Test
+    fun reverseRestaurantList() {
+    }
+}
